4c3d91d85ad05fd8b7156618783c48d3ff164f14
Your Name <you@example.com>
2023-05-27 02:34:53 -0500

    update

--- GRPC/main_client.py
+++ GRPC/main_client.py
@@ -5,12 +5,13 @@ from sys import argv
 from proto_grpc import userexample_pb2
 from proto_grpc import userexample_pb2_grpc
 
-GRPC_HOST = argv[1] if len(argv[1]) > 1 else os.getenv('GRPC_HOST', '127.0.0.1')  # local.poxsilver5.store
-GRPC_PORT = argv[2] if len(argv[2]) > 2 else os.getenv('GRPC_PORT', '50051')
-CERTFILE_CRT = argv[3] if len(argv[3]) > 3 else os.getenv('CERTFILE_CRT', './.certs/tls.crt')
-KEYFILE_TLS = argv[4] if len(argv[4]) > 4 else os.getenv('KEYFILE_TLS', './.certs/tls.key')
-CA_CERT_TLS = argv[5] if len(argv[5]) > 5 else os.getenv('CA_CERT_TLS', './.certs/chain.pem')
-
+GRPC_HOST = argv[1] if len(argv) > 1 else os.getenv('GRPC_HOST', '127.0.0.1')  # local.poxsilver5.store
+GRPC_PORT = argv[2] if len(argv) > 2 else os.getenv('GRPC_PORT', '50051')
+CERTFILE_CRT = argv[3] if len(argv) > 3 else os.getenv('CERTFILE_CRT', './.certs/tls.crt')
+KEYFILE_TLS = argv[4] if len(argv) > 4 else os.getenv('KEYFILE_TLS', './.certs/tls.key')
+CA_CERT_TLS = argv[5] if len(argv) > 5 else os.getenv('CA_CERT_TLS', './.certs/chain.pem')
+# cert.conf  flask-any-service-a.default.svc.crt  flask-any-service-a.default.svc.key  rootCA.key  server.csr  ssl.sh   tls.key
+# csr.conf   flask-any-service-a.default.svc.csr  rootCA.crt                           server.crt  server.key  tls.crt  tls.srl
 
 def run(user_name="John Doe", age=30, email="johndoe@example.com"):
     # set tls
